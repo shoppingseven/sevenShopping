40a4add54d67f70cca673340e9d151c84186df32
xiaodongmemeda <344650464@qq.com>
2017-07-11 21:40:06 +0800

    ht

---
+++ app/src/main/java/qiuhaitao/bwie/com/mall/view/activity/BuySetupActivity.java
@@ -0,0 +1,30 @@
+package qiuhaitao.bwie.com.mall.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import qiuhaitao.bwie.com.mall.R;
+
+/**
+ * Created by 仇海涛 on 2017/7/11.
+ * class ：
+ * content ：
+ */
+
+public class BuySetupActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_setup);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+
+    }
+}
